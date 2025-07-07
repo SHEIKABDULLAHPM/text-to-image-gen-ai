36f703e7e674400e741ddd83a8df2efc88f470ac
sheikabdullahpm <sheikabdullahpeer@gmail.com>
2025-07-07 20:08:17 +0530

    final project

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   plugins: [react()],
+  build: {
+    outDir: 'dist', // <-- this is default but make sure it's present
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
